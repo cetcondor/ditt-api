6d38c1ed9d3e2125636120ea0b90112554818d7a
Bedřich Schindler <bedrich@visionapps.cz>
2019-02-27 08:49:39 +0100

    DITT-125: Create dependent entities when new supported year is created

--- src/Service/ConfigService.php
+++ src/Service/ConfigService.php
@@ -5,8 +5,12 @@ namespace App\Service;
 use App\Entity\Config;
 use App\Entity\SupportedHoliday;
 use App\Entity\SupportedYear;
+use App\Entity\UserYearStats;
+use App\Entity\WorkHours;
+use App\Entity\WorkMonth;
 use App\Repository\SupportedHolidayRepository;
 use App\Repository\SupportedYearRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
@@ -39,24 +43,32 @@ class ConfigService
     private $supportedHolidayRepository;
 
     /**
+     * @var UserRepository
+     */
+    private $userRepository;
+
+    /**
      * @param DenormalizerInterface $denormalizer
      * @param EntityManagerInterface $entityManager
      * @param ValidatorInterface $validator
      * @param SupportedYearRepository $supportedYearRepository
      * @param SupportedHolidayRepository $supportedHolidayRepository
+     * @param UserRepository $userRepository
      */
     public function __construct(
         DenormalizerInterface $denormalizer,
         EntityManagerInterface $entityManager,
         ValidatorInterface $validator,
         SupportedYearRepository $supportedYearRepository,
-        SupportedHolidayRepository $supportedHolidayRepository
+        SupportedHolidayRepository $supportedHolidayRepository,
+        UserRepository $userRepository
     ) {
         $this->denormalizer = $denormalizer;
         $this->entityManager = $entityManager;
         $this->validator = $validator;
         $this->supportedYearRepository = $supportedYearRepository;
         $this->supportedHolidayRepository = $supportedHolidayRepository;
+        $this->userRepository = $userRepository;
     }
 
     /**
@@ -145,14 +157,20 @@ class ConfigService
             // Remove all existing holidays
             $platform = $connection->getDatabasePlatform();
             $connection->executeQuery($platform->getTruncateTableSQL('supported_holiday'));
-            $this->entityManager->flush();
 
             // Persist new supported holidays
             foreach ($supportedHolidays as $newSupportedHoliday) {
                 $this->entityManager->persist($newSupportedHoliday);
-                $this->entityManager->flush();
             }
 
+            // Generate and persist all new dependent entities
+            foreach ($newSupportedYears as $newSupportedYear) {
+                foreach ($this->generateNewEntitiesWithYear($newSupportedYear) as $newEntity) {
+                    $this->entityManager->persist($newEntity);
+                }
+            }
+
+            $this->entityManager->flush();
             $connection->commit();
         } catch (\InvalidArgumentException $exception) {
             $connection->rollback();
@@ -170,5 +188,36 @@ class ConfigService
         }
 
         return $this->getConfig();
+    }
+
+    /**
+     * @param SupportedYear $supportedYear
+     * @return array
+     */
+    private function generateNewEntitiesWithYear(SupportedYear $supportedYear): array
+    {
+        $users = $this->userRepository->getRepository()->findAll();
+        $newEntities = [];
+
+        foreach ($users as $user) {
+            for ($month = 1; $month <= 12; ++$month) {
+                $newEntities[] = (new UserYearStats())
+                    ->setYear($supportedYear)
+                    ->setUser($user);
+
+                $newEntities[] = (new WorkHours())
+                    ->setYear($supportedYear)
+                    ->setMonth($month)
+                    ->setRequiredHours(0)
+                    ->setUser($user);
+
+                $newEntities[] = (new WorkMonth())
+                    ->setYear($supportedYear)
+                    ->setMonth($month)
+                    ->setUser($user);
+            }
+        }
+
+        return $newEntities;
     }
 }
